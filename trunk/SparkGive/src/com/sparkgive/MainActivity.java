41ac69fd76f84a12527e35c5feda73efcb9da508
namrata.bandekar@gmail.com <namrata.bandekar@gmail.com@dea924af-9928-d3ee-8a07-5eb932b691be>
2012-12-01 21:24:04 +0000

    hooking up footer buttons to activities

--- trunk/SparkGive/src/com/sparkgive/MainActivity.java
+++ trunk/SparkGive/src/com/sparkgive/MainActivity.java
@@ -4,6 +4,7 @@ import com.sparkgive.model.SparkGiveModel;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -13,11 +14,15 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements OnClickListener {
 
+	private ImageView profileButton, mapsButton, publicCardsButton, settingsButton;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	
@@ -25,10 +30,41 @@ public class MainActivity extends FragmentActivity {
     	SparkGiveModel.nearByPlaces.add("Place 2");
     	SparkGiveModel.nearByPlaces.add("Place 3");
     	
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
+        profileButton = (ImageView) findViewById(R.id.profile);
+        mapsButton = (ImageView) findViewById(R.id.Maps_button);
+        publicCardsButton = (ImageView) findViewById(R.id.public_cards);
+        settingsButton = (ImageView) findViewById(R.id.list);
+        
+        profileButton.setOnClickListener(this);
+        mapsButton.setOnClickListener(this);
+        publicCardsButton.setOnClickListener(this);
+        settingsButton.setOnClickListener(this);
+        
     }
 
+    @Override
+	public void onClick(View v) {
+		
+		switch (v.getId()) {
+		case R.id.profile:
+			startActivity(new Intent(getApplicationContext(), CampaignsActivity.class));
+			break;
+		case R.id.Maps_button:
+			startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+			break;	
+		case R.id.public_cards:
+			startActivity(new Intent(getApplicationContext(), PublicCardsActivity.class));
+			break;
+		case R.id.list:
+			//startActivity(new Intent(getApplicationContext(), CampaignsActivity.class));
+			break;
+		}
+    }
+
+    
 
 }
---
+++ trunk/SparkGive/src/com/sparkgive/PublicCardsActivity.java
@@ -0,0 +1,22 @@
+package com.sparkgive;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class PublicCardsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_campaigns);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_campaigns, menu);
+		return true;
+	}
+
+}
