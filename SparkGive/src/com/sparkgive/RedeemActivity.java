894f8df70bfddb550e5911cfcae3071e019a50a3
namrata.bandekar@gmail.com <namrata.bandekar@gmail.com@dea924af-9928-d3ee-8a07-5eb932b691be>
2012-12-01 23:25:21 +0000

    Added search bar and home button functionality for all activities

--- SparkGive/src/com/sparkgive/RedeemActivity.java
+++ SparkGive/src/com/sparkgive/RedeemActivity.java
@@ -2,7 +2,9 @@ package com.sparkgive;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class RedeemActivity extends Activity {
 
@@ -10,6 +12,8 @@ public class RedeemActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_redeem);
+		// enables the activity icon as a 'home' button. required if "android:targetSdkVersion" > 14
+        getActionBar().setHomeButtonEnabled(true);
 	}
 
 	@Override
@@ -18,5 +22,20 @@ public class RedeemActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_redeem, menu);
 		return true;
 	}
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) 
+    {    
+       switch (item.getItemId()) 
+       {        
+          case android.R.id.home:            
+             Intent intent = new Intent(this, MainActivity.class);            
+             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); 
+             startActivity(intent);            
+             return true;        
+          default:            
+             return super.onOptionsItemSelected(item);    
+       }
+    }
 
 }
