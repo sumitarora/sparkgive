987102ba35fc1276cd07ca67b4890b4850baafbe
namrata.bandekar@gmail.com <namrata.bandekar@gmail.com@dea924af-9928-d3ee-8a07-5eb932b691be>
2012-12-02 17:58:35 +0000

    graphics

--- SparkGive/src/com/sparkgive/model/SparkGiveModel.java
+++ SparkGive/src/com/sparkgive/model/SparkGiveModel.java
@@ -15,10 +15,10 @@ public class SparkGiveModel
 	public static ArrayList<Card> cardList = new ArrayList<Card>() {{ 
 	add(new Card("details for food banks and PC", "food banks",R.drawable.foodbankcanada,R.drawable.back_sickkids));
 	add(new Card("details for sick kids and shoppers", "sick kids", R.drawable.back_sickkids, R.drawable.back_sickkids));
-	add(new Card("details for breast cancer foundation and starbucks", "breast cancer", R.drawable.back_starbucks, R.drawable.back_sickkids));
+	add(new Card("Starbucks will donate 2% of your purchase price, given a purchase of $5 or more, to the Canadian Breast Cancer Research Foundation. SparkCard expires on June 1, 2013.", "breast cancer", R.drawable.back_starbucks, R.drawable.back_sickkids));
 	add(new Card("details for PC", "food banks",R.drawable.back_walmart, R.drawable.back_sickkids));
 	add(new Card("details for shoppers drug mart", "sick kids", R.drawable.back_loblaws, R.drawable.back_sickkids));
-	add(new Card("details for starbucks", "breast cancer", R.drawable.back_toysrus, R.drawable.back_sickkids));
+//	add(new Card("details for starbucks", "breast cancer", R.drawable.back_toysrus, R.drawable.back_sickkids));
 	}};
 	
 }
